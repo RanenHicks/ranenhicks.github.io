825f354ec233c5a924fbbb64a56e29030cde71cc
RanenHicks <125837194+RanenHicks@users.noreply.github.com>
2024-08-18 18:17:28 -0700

    Delete Databases/CS-499 - Databases - Ranen Hicks directory

--- Databases/CS-499 - Databases - Ranen Hicks/CS499DatabasesRanenHicks/database/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-# (Mashutin, 2024; Writing Your First Django App, Part 1, n.d.)
-urlpatterns = [
-    path("", views.showLink, name = 'index'),
-]
