5bf7f2dab3e0b1f703319bfba960941cff199cff
RanenHicks <125837194+RanenHicks@users.noreply.github.com>
2024-08-18 18:18:54 -0700

    Delete Databases/CS-340 ProjectTwo Artifact - Ranen Hicks/CS499DatabasesRanenHicks directory

--- Databases/CS-340 ProjectTwo Artifact - Ranen Hicks/CS499DatabasesRanenHicks/database/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# (Mashutin, 2024; Writing Your First Django App, Part 1, n.d.), Renders the first database with button to datatable
-def showLink(request):
-    return render(request, "templates/homepage.html")
-
